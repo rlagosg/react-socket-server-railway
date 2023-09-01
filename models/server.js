b7d2a9f31f0b38fbe39e9ce13a62f4d25abf9b8e
rlagosg <rlagosg@yahoo.com>
2023-08-31 23:20:46 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,47 @@
+// Servidor de Express
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const path     = require('path');
+
+const Sockets = require('./sockets');
+
+class Server {
+
+    constructor(){
+
+        this.app    = express();
+        this.port   = process.env.port;
+
+        // Http server
+        this.server = http.createServer(this.app);
+
+        // configuracion de Socket
+        this.io = socketio(this.server, { /*  */});
+    }
+
+    middlewares(){
+        //Desplegar el directorio publico
+        this.app.use( express.static( path.resolve( __dirname, '../public')));
+    }
+
+    configurarSockets(){
+        new Sockets(this.io);
+    }
+
+    execute(){
+
+        //Inicializar mimddle
+        this.middlewares();
+
+        this.configurarSockets();
+
+        //Inicializar Server
+        this.server.listen( this.port, () => { 
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+         });
+    }
+
+}
+
+module.exports = Server;
